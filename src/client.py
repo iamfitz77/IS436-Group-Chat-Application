8208b98b0bc95870ab8d104cc0604469f912a057
iamfitz77 <fafarim1@umbc.edu>
2026-04-09 20:50:12 -0400

    Create client.py
    Add starter client file

---
+++ src/client.py
@@ -0,0 +1,16 @@
+"""
+IS 436 Group Project
+Basic Chat Application - Client
+Starter file for checkpoint progress
+"""
+
+import socket
+import sys
+
+
+def main():
+    print("Client starter file created.")
+
+
+if __name__ == "__main__":
+    main()
