045f4dbd41323d44970488402ed27485740b66f9
iamfitz77 <fafarim1@umbc.edu>
2026-04-09 20:48:43 -0400

    Create server.py
    Add starter server file

---
+++ src/server.py
@@ -0,0 +1,16 @@
+"""
+IS 436 Group Project
+Basic Chat Application - Server
+Starter file for checkpoint progress
+"""
+
+import socket
+import sys
+
+
+def main():
+    print("Server starter file created.")
+
+
+if __name__ == "__main__":
+    main()
